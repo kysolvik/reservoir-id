f598e0cb8f4d2637c302926f9d65158b2569ed67
Kylen Solvik <ksolvik@kylensmbp.whrc.ad>
2017-04-05 11:14:42 -0400

    Added some derivative features (e.g. contour area/convex hull area)

--- add_features/bounding_geo_props.py
+++ add_features/bounding_geo_props.py
@@ -10,13 +10,15 @@ import numpy as np
 import scipy.misc
 import gdal
 import pandas as pd
+import math
 
 # Set some input variables
-im_path = '/Users/ksolvik/Documents/Research/MarciaWork/data/build_attribute_table/wat_only_clip_point.tif'
-prop_outpath = '/Users/ksolvik/Documents/Research/MarciaWork/data/build_attribute_table/att_table.csv'
-cont_outpath = '/Users/ksolvik/Documents/Research/MarciaWork/data/build_attribute_table/contours.csv'
+im_path = '/Users/ksolvik/Documents/Research/MarciaWork/data/shapeAnalysis/wat_only_morph.tif'
+prop_outpath = '/Users/ksolvik/Documents/Research/MarciaWork/data/build_attribute_table/att_table_foo.csv'
+cont_outpath = '/Users/ksolvik/Documents/Research/MarciaWork/data/build_attribute_table/contours_foo.csv'
 shapenames = ["obj","approx","hull","rect"]
 
+triangle_shape = np.asarray([[[0,0]], [[4,0]], [[2,6]]])
 # Function to read in image and save as array
 def read_image(filepath):
     file_handle = gdal.Open(filepath)
@@ -33,6 +35,12 @@ def cont_features(obj):
     # Object moments
     shp_feats = cv2.moments(obj)
 
+    # Match with triangle shape
+    for match_meth in range(1,4):
+        shp_feats['tri_match'+str(match_meth)] = cv2.matchShapes(obj,triangle_shape,match_meth,0)
+
+
+        
     # Get areas and all that good stuff
     for shp in shapenames :
         shp_feats[shp + "_area"] = cv2.contourArea(shp_dict[shp])
@@ -40,16 +48,24 @@ def cont_features(obj):
         
     # Circle is separate because it's a little messier
     (circx,circy),circradius = cv2.minEnclosingCircle(obj)
-    shp_feats["circ_radius"] = circradius
+    shp_feats["circ_rad"] = circradius
 
     # Return dictionary
     return(shp_feats)
     
+# Funciton to caclulate derived features
+def derived_features(fd):
+    fd['circ_rsq'] = math.pow(fd['circ_rad'],2)
+    fd['eq_diam'] = math.sqrt((4*fd['obj_area'])/math.pi)
+    fd['solidity'] = fd['obj_area']/fd['hull_area']
+    return(fd)
+
 
 def main():
     # Read image
     wat_im = read_image(im_path)
 
+    # Read in 
     # Get contours
     im2, contours, hierarchy = cv2.findContours(wat_im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -59,7 +75,10 @@ def main():
     cont_id = 0
     for cnt in contours:
         feat_dict = cont_features(cnt)
-
+        if feat_dict['obj_area']==0:
+            continue
+        feat_dict = derived_features(feat_dict)
+        
         if 'prop_df' not in locals():
             colnames = ["id"] + feat_dict.keys()
             prop_df = pd.DataFrame(columns = colnames)
@@ -67,10 +86,10 @@ def main():
         prop_df.loc[cont_id,colnames] = [cont_id] + feat_dict.values()
         cont_df.loc[cont_id] = [cont_id] + [cnt.tolist()]
 
+        
+        
         cont_id+=1
-    print(prop_df.head())
-    print(cont_df.head())
-
+        print(cont_id)
     prop_df.to_csv(prop_outpath)
     cont_df.to_csv(cont_outpath)
     
