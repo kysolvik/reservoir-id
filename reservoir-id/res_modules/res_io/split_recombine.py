b36737daa39c8cd8b20f752bc2bd78f0cfb47af0
Kylen Solvik <kysolvik@gmail.com>
2017-05-23 15:42:06 -0400

    split in parallel. Runs ~75% faster on 7 cpus

--- reservoir-id/res_modules/res_io/split_recombine.py
+++ reservoir-id/res_modules/res_io/split_recombine.py
@@ -2,6 +2,7 @@
 
 import os
 import gdal
+import multiprocessing as mp
 
 def split_raster(in_tif,out_dir,out_prefix,tile_size_x,tile_size_y,
                  overlap_size):
@@ -15,10 +16,11 @@ def split_raster(in_tif,out_dir,out_prefix,tile_size_x,tile_size_y,
         xsize = band.XSize
         ysize = band.YSize
 
-        # Create tiles
+        #Create queue of split commands to run
+        command_list = []
         for i in range(0, xsize, (tile_size_x-overlap_size)):
                 for j in range(0, ysize, (tile_size_y-overlap_size)):
-                        com_string = "gdal_translate -of GTIFF -srcwin " + \
+                        com_string = "gdal_translate -q -of GTIFF -srcwin " + \
                                      str(i) + ", " + str(j) + ", " + \
                                      str(tile_size_x) + ", " \
                                      + str(tile_size_y) \
@@ -26,15 +28,18 @@ def split_raster(in_tif,out_dir,out_prefix,tile_size_x,tile_size_y,
                                      + " " + str(out_dir) + "/" \
                                      + str(out_prefix) + \
                                      str(i) + "_" + str(j) + ".tif"
-                        os.system(com_string)
+                        command_list.append(com_string)
 
+        # Run all commands
+        pool = mp.Pool(mp.cpu_count()-2)
+        pool.map(os.system,command_list)
 
         return("Done with split")
 
 def recombine_raster(in_dir,in_prefix,out_tif):
         # Run command
-        com_string = "gdalwarp -r near -wm 3000 -overwrite " + in_dir + "/" + in_prefix + "* " \
-                     + out_tif
+        com_string = "gdalwarp -r near -wm 3000 -overwrite " + in_dir + "/" + \
+                     in_prefix + "* " + out_tif
         print(com_string)
         os.system(com_string)
         
