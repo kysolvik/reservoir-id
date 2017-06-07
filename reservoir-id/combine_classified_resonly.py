18e6a8af3073d4eb98f510687685d1c724fec1fc
Kylen Solvik <kysolvik@gmail.com>
2017-06-07 12:16:47 -0400

    combine_classified_resonly no longer overwrites classified tifs

--- reservoir-id/combine_classified_resonly.py
+++ reservoir-id/combine_classified_resonly.py
@@ -12,12 +12,14 @@ import glob
 def combine_classified(class_pattern,res_class,output_vrt):
     # First, gdal calc all outputs to only include reservoirs
     for tile_path in glob.glob(class_pattern):
+        tile_noext = path.splitext(tile_path)[0]
         os.system("gdal_calc.py -A " + tile_path + " --calc='1*(A==" +
                   res_class + ")' --overwrite " +
                   "--creation-option='COMPRESS=LZW' " +
-                  "--outfile=" + tile_path)
+                  "--outfile=" + tile_noext + "_resonly.tif")
     # Build vrt
-    os.system("gdalbuildvrt " + output_vrt + " " + class_pattern)
+    resonly_pattern = path.dirname(class_pattern) + "_resonly.tif"
+    os.system("gdalbuildvrt " + output_vrt + " " + resonly_pattern)
               
 def main():
     combine_classified(sys.argv[1],sys.argv[2],sys.argv[3])
