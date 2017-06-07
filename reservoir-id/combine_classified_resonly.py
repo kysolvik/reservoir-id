c423eb9fc9274c706b085d7e7b273eb84b95b103
Kylen Solvik <kysolvik@gmail.com>
2017-06-07 12:32:54 -0400

    Added overwrite resonly tiles y/n option to combine_classified_resonly

--- reservoir-id/combine_classified_resonly.py
+++ reservoir-id/combine_classified_resonly.py
@@ -9,20 +9,22 @@ import gdal
 from os import path
 import glob
 
-def combine_classified(class_pattern,res_class,output_vrt):
+def combine_classified(class_pattern,res_class,output_vrt,overwrite):
     # First, gdal calc all outputs to only include reservoirs
     for tile_path in glob.glob(class_pattern):
-        tile_noext = path.splitext(tile_path)[0]
-        os.system("gdal_calc.py -A " + tile_path + " --calc='1*(A==" +
-                  res_class + ")' --overwrite " +
-                  "--creation-option='COMPRESS=LZW' " +
-                  "--outfile=" + tile_noext + "_resonly.tif")
+        if "resonly" not in tile_path:
+            tile_noext = path.splitext(tile_path)[0]
+            if overwrite or not path.isfile(tile_noext + "_resonly.tif"):
+                os.system("gdal_calc.py -A " + tile_path + " --calc='1*(A==" +
+                          res_class + ")' --overwrite " +
+                          "--creation-option='COMPRESS=LZW' " +
+                          "--outfile=" + tile_noext + "_resonly.tif")
     # Build vrt
-    resonly_pattern = path.dirname(class_pattern) + "_resonly.tif"
+    resonly_pattern = path.dirname(class_pattern) + "/*_resonly.tif"
     os.system("gdalbuildvrt " + output_vrt + " " + resonly_pattern)
               
 def main():
-    combine_classified(sys.argv[1],sys.argv[2],sys.argv[3])
+    combine_classified(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]=="True")
     return()
 
 if __name__ == '__main__':
