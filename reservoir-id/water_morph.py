222f5fbf48b94e995e9f7212446c5debd13b2ca9
Kylen Solvik <kysolvik@gmail.com>
2017-07-13 14:54:39 -0400

    Renamed grow_shrink_input to water_morph. Now uses argparse

--- reservoir-id/water_morph.py
+++ reservoir-id/water_morph.py
@@ -17,18 +17,28 @@ import cv2
 import numpy as np
 import scipy.misc
 import gdal
+import argparse
 
 from res_modules.res_io import read_write
 
 # Parse command line args
-land_cover_path = sys.argv[1]
-water_class = 1
-morph_truefalse = sys.argv[2]
-out_tif = sys.argv[3]
+parser = argparse.ArgumentParser(description='Dilate then erode water objects.')
+parser.add_argument('landcover_tif',help='Path to landcover tif with water class',
+                    type=str)
+parser.add_argument('out_tif',
+                    help='Path for output tif with morphed water',
+                    type=str)
+parser.add_argument('--water_class',
+                    help='Integer water class in landcover_tif. Default=1.',
+                    default=1,type=int)
+parser.add_argument('--no_morph',
+                    action="store_true",
+                    help='Skip morphology, output unmodifed water objects.')
+args = parser.parse_args()
 
 def grow_shrink(lc_array):
     # Recode non-water to 0
-    lc_watonly = np.where(lc_array == water_class, 1, 0)
+    lc_watonly = np.where(lc_array == args.water_class, 1, 0)
     # Execute shrink/grow
     #se = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
     se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
@@ -40,10 +50,10 @@ def grow_shrink(lc_array):
 
 def main():
     # Read
-    lc,geotrans = read_write.read_image(land_cover_path)
+    lc,geotrans = read_write.read_image(args.landcover_tif)
     print(lc)
     # Grow/shrink
-    if morph_truefalse:
+    if not args.no_morph:
         lc = grow_shrink(lc)
 
     # Print out some basic info
@@ -52,7 +62,7 @@ def main():
     print(lc.nbytes)
     
     # Write out to geotiff
-    read_write.write_image(lc,land_cover_path,out_tif,gdal.GDT_Byte)
+    read_write.write_image(lc,args.landcover_tif,args.out_tif,gdal.GDT_Byte)
 
     
 if __name__ == '__main__':
